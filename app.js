bbf9387a42e35972509982e0c09dd2f6bb436fe1
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-28 11:49:07 -0230

    2nd Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const fileName = "file 1";
+const filePath = path.resolve(__dirname, fileName);
+
