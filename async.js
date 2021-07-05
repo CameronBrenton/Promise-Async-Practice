57a493a78cceeff0a4e4c84eff577866a7f02924
CameronBrenton <Cameron.Brenton@keyin.com>
2021-07-05 14:25:43 -0230

    Jul 5: 2:25pm commit

--- async.js
+++ async.js
@@ -45,18 +45,83 @@ readFileWrappedInPromise(filePath)
 });
 
 /* Use 2: A function that returns a promise because it uses the 'async' keyword */
-async function testAsync() {
-	try {
+async function testAsync2() {
+	try { //Try to do something
 		console.log('Before doing anything');
 		const data = await readFileWrappedInPromise(filePath);
 		console.log('data: ', data);
+	}
+	catch (error) { // Catch any errors that may have occurred during the 'try'
+		console.error(error);
+	}
+	finally { // Regardless of what happens, do the following
+		console.log("Finished read file operation");
+	}
+}
+
+testAsync();
+
+/* Approach 2: convert async code to a Promise using util.promisify */
+const readFilePromisified = util.promisify(fs.readFile);
+
+readFilePromisified(filePath, "UTF-8")
+.then(data => {
+	console.log("[1] Printing the data");
+	console.log(data.substr(0, 10));
+})
+.catch(error => {
+	console.log("[2] Handling the error");
+	console.error(error);
+});
+
+/* Usage v2: A function that uses await to execute the promisified function */
+async function testAsync2() {
+	try {
+		console.log("before doing anything");
+		const data = await readFilePromisified(filePath, "UTF-8");
+		console.log("data: ", data.substr(0, 10));
+
+		return data.substr(0, 10);
 	}
 	catch (error) {
 		console.error(error);
 	}
 	finally {
-		console.log("Finished read fil operation");
+		console.log("Finished read file operation");
 	}
 }
 
-testAsync();
+/* Usage v2a: .then style */
+testAsync2()
+.then(substr => {
+	console.log("returned data: ", substr);
+})
+
+// Usage v2b: Anonymous/root function call
+(async function () {
+	const substr = await testAsync2();
+	console.log("Returned data: ", substr);
+})()
+
+// Demonstration of how to write promises and async code
+
+function getUsers() {
+	return new Promise(function(resolve, reject) {
+	    // do stuff
+	});
+}
+    
+const getUsers = () => {
+	return new Promise(function(resolve, reject) {
+	    // do stuff
+	});
+}
+    
+async function getUsers() {
+	// do stuff
+	// await 
+}
+    
+const getUsers = async () => {
+	// do stuff
+};
